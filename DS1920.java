e00d7b6f0347acc39a9d4ba44661dc223db3e233
Gorish Gupta <gorishgupta50@gmail.com>
2025-05-06 23:35:27 +0530

    Update DS1920.java

--- DS1920.java
+++ DS1920.java
@@ -6,4 +6,13 @@ public class DS1920 {
         }
         return temp;
     }
+
+    public static void main(String[] args) {
+        int arr[] = { 0, 2, 1, 5, 3, 4 };
+        DS1920 ds = new DS1920();
+        int result[] = ds.builtArray(arr);
+        for (int i = 0; i < result.length; i++) {
+            System.out.print(result[i] + " ");
+        }
+    }
 }
