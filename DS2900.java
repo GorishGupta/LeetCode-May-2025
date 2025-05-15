b3c61292e1b30f67ce9f79009f483d1ef74f62e9
Gorish Gupta <gorishgupta50@gmail.com>
2025-05-15 20:07:34 +0530

    Create DS2900.java

---
+++ DS2900.java
@@ -0,0 +1,15 @@
+public class DS2900 {
+    public List<String> getLongestSubsequence(String[] words, int[] groups) {
+        List<String> result = new ArrayList<>();
+        int prev = -1;
+
+        for (int i = 0; i < groups.length; i++) {
+            if (groups[i] != prev) {
+                prev = groups[i];
+                result.add(words[i]);
+            }
+        }
+
+        return result;
+    }
+}
