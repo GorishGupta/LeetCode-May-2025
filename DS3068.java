fcd877174947295a3d434b1013ea7f029c37f419
Gorish Gupta <gorishgupta50@gmail.com>
2025-05-23 23:35:24 +0530

    Update DS3068.java

--- DS3068.java
+++ DS3068.java
@@ -11,6 +11,11 @@ public class DS3068 {
     }
 
     public static void main(String[] args) {
+        DS3068 ds = new DS3068();
+        int[] nums = { 1, 2, 3 };
+        int k = 2;
+        int[][] edges = { { 0, 1 }, { 1, 2 } };
+        System.out.println(ds.maximumValueSum(nums, k, edges)); // Output: 6
 
     }
 }
